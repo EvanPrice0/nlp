0c276e693896e5688d44b3d2229d01d6a005ab2f
EvanPrice0 <evan.a.price.0@gmail.com>
2022-04-25 11:03:55 -0400

    ADD: got it working

--- nlp_analytic.py
+++ nlp_analytic.py
@@ -1,8 +1,8 @@
 import mechanicalsoup
 from bs4 import BeautifulSoup
-from decouple import config
 from urllib.request import urlopen
 
 url= "https://www.imf.org/en/Publications/Search?series=IMF%20Staff%20Country%20Reports&when=During&year=2022&title=Kenya"
 browser = mechanicalsoup.Browser()
 login_page = browser.get(url)
+print(login_page.content)
